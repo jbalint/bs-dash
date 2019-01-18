7c94bf7ac605ed99d9a9a52ef5d65fa8ba8f26a1
Jess Balint <jbalint@gmail.com>
2019-01-18 16:35:34 -0600

    demo code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use termion::raw::IntoRawMode;
+use tui::backend::TermionBackend;
+use tui::Terminal;
+use tui::widgets::Block;
+use tui::widgets::Borders;
+use tui::widgets::Widget;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    let size = terminal.size()?;
+    terminal.draw(|mut f| {
+        Block::default()
+            .title("Block")
+            .borders(Borders::ALL)
+            .render(&mut f, size);
+    })
 }
