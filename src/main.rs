3c967d80e357bddb718e0731a02ce88fa7c76818
Jess Balint <jbalint@gmail.com>
2019-01-20 16:54:35 -0600

    use input keys to block repaints

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,78 @@
+use std::fs::read_dir;
 use std::io;
-use std::iter;
-use std::thread;
-use std::time::Duration;
-use std::borrow::Borrow;
+use std::io::stdin;
 
+use itertools::Itertools;
 use termion::clear;
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use tui::backend::Backend;
 use tui::backend::TermionBackend;
+use tui::layout::Rect;
 use tui::Terminal;
 use tui::widgets::Block;
 use tui::widgets::Borders;
-use tui::widgets::canvas::Line;
 use tui::widgets::Paragraph;
 use tui::widgets::Text;
 use tui::widgets::Widget;
-use std::fs::read_dir;
-use std::collections::HashMap;
-use std::iter::Map;
-use std::iter::Take;
-use std::fs::ReadDir;
-use std::fs::DirEntry;
-use std::io::Error;
-use std::ffi::OsString;
+use tui::style::Style;
+use tui::style::Color;
+
+fn draw_files<T: Backend>(terminal: &mut Terminal<T>, area: Rect) -> io::Result<()> {
+    terminal.draw(|mut f| {
+        let block = Block::default()
+            .style(Style::default().bg(Color::White))
+            .title("Block")
+            .borders(Borders::ALL);
+        // this is kind of stupid, but we create a /sorted/ list of strings
+        // before turning them into Text objects
+        // (sorted() produces a Vec which must be saved into a variable,
+        //  and we need a Vec of Text to create an iterator of Text references
+        //  for Paragraph::new())
+        let filename_strings =
+            read_dir("/home/jbalint/dl")
+                .unwrap()
+                .take(10)
+                .map(|entry| entry.unwrap().file_name())
+                .map(|name| String::from(name.to_string_lossy()))
+                .map(|mut str| {
+                    str.push('\n');
+                    str
+                })
+                .sorted();
+        let files = filename_strings
+            .iter()
+            .map(|str| Text::raw(str))
+            .collect::<Vec<Text>>();
+        Paragraph::new(files.iter())
+            .block(block)
+            .wrap(true)
+            .render(&mut f, area);
+    })
+}
 
 fn main() -> Result<(), io::Error> {
     //let test = HashMap::new();
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
     let size = terminal.size()?;
-    println!("{}", clear::All);
-    terminal.draw(|mut f| {
-        let mut block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
-        block.render(&mut f, size);
 
-        //let files: Map<Map<Take<ReadDir>, fn(Result<DirEntry, Error>) -> OsString>, fn(OsString) -> Text> =
-        let files =
-            read_dir("/home/jbalint/dl")
-                .unwrap()
-                .take(10)
-                .map(|entry| entry.unwrap().file_name())
-                .map(|name| String::from(name.to_string_lossy()))
-                .map(|mut str| { str.push('\n'); str })
-                .map(|str| Text::raw(str))
-                .collect::<Vec<Text>>();
-        Paragraph::new(files.iter())
-            .block(block)
-            .render(&mut f, size);
-//        Paragraph::new(iter::once(&Text::raw(String::from("asdf"))))
-//            .block(block)
-//            .render(&mut f, size);
-    })?;
-    //loop{}
-    thread::sleep(Duration::from_millis(5000));
+    //let mut selected_file = 0;
+
+    println!("{}", clear::All);
+
+    for c in stdin().keys() {
+        draw_files(&mut terminal, size)?;
+
+        match c.unwrap() {
+            Key::Char('q') => break,
+            //Key::Up => { selected_file += 1 }
+            _ => {}
+        }
+    }
+
     Ok(())
 }
